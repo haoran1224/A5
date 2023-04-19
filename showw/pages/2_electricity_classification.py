503a7c79b22553b4addf8744e67db468bf53508e
吕浩然 <2240860990@qq.com>
2023-04-19 13:55:28 +0800

    第三次修改

--- showw/pages/2_electricity_classification.py
+++ showw/pages/2_electricity_classification.py
@@ -3,12 +3,76 @@ import pandas as pd
 import numpy as np
 import tslearn.metrics as metrics
 import matplotlib.pyplot as plt
+from lttb import lttb
 from tslearn.clustering import TimeSeriesKMeans
 
 from electricity_classification.util import get_data, jiangwei
 
 means = []
 
+# 读取数据
+def get_data(data):
+    # data = read_csv("2015.01.01—2016.08.31中国江苏省扬中市1000多家企业每日用电量数据.csv")
+    df = pd.DataFrame(data, columns=['user_id', 'record_date', 'power_consumption'])
+    print(df.head())
+
+    # 查看数据量
+    print("当前数据集含有%s行,%s列" % (df.shape[0], df.shape[1]))
+
+    # 查看id，可知不同的用户共有1454户
+    print(df['user_id'].value_counts())
+
+    # 改变数据类型
+    df.loc[:, 'power_consumption'] = df.power_consumption.astype(float)
+    df.loc[:, 'user_id'] = df['user_id'].astype(int)
+    df.loc[:, 'record_date'] = pd.to_datetime(df.record_date)
+
+    # 上述数据的时间仅为一列，无法较好的反映工作日与周末及每月具体日期的区别，因此尝试添加列进一步细化日期
+    # 添加一代表星期的列，isoweekday会根据日期判定是周几
+    df.loc[:, 'type_day'] = df.record_date.apply(lambda x: x.isoweekday())
+
+    # 添加一代表日期的列，day会根据具体日期判定是几号
+    df.loc[:, 'day_of_month'] = df.record_date.apply(lambda x: x.day)
+
+    # 按照id和日期进行重新排序
+    df = df.sort_values(['user_id', 'record_date'], ascending=[True, True])
+    df = df.reset_index(drop=True)
+    print(df.head())
+
+    # 筛选出工作日
+    df0 = df[df.type_day <= 5]
+
+    # 按照日期和时间绘制数据透视表，获得不同时间下的用户用电数据
+    df0 = pd.pivot_table(data=df, columns=['record_date'], values='power_consumption', index=['user_id'])
+    print(df0.head())
+
+    # 用户用电曲线
+    # fig, ax = plt.subplots(figsize=(16, 9))
+    # plt.plot(df0.T, alpha=1, lw=1)
+    # plt.ylabel('KW')
+    # plt.show()
+    return df0
+
+
+# total (1454,609)----->(1454,50)进行了数据的降维
+def jiangwei(data):
+    # 数组标记
+    # 一行一共有多少数据
+    tre = np.array([range(data.shape[1])])
+
+    # 共有多少组数据
+    num = data.shape[0]
+    # 声明一个空数组
+    total = np.empty((num, 50))
+
+    for j in range(num):
+        temp = np.dstack((tre, data[j]))
+        # 将第一步的一维降维
+        temp = np.squeeze(temp)
+        small_data = lttb.downsample(temp, n_out=50)
+        total[j] = small_data[:, 1]
+    print('降维结束')
+    return total
 
 def label_predict(user_data):
     # 数据预处理
--- showw/pages/3_test.py
+++ showw/pages/3_test.py
@@ -2,11 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_1():
-    arr = np.random.normal(1, 1, size=100)
-    fig, ax = plt.subplots()
-    ax.hist(arr, bins=20)
-    return fig
+import sys
 
-
-st.pyplot(plot_1())
+sys.path.append(' ')
+print(sys.path)
